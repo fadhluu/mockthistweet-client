6d6438f7b3149f6e13ad4cad3690a67e48395be5
fadhlu <fadhlurahman.i@gmail.com>
2020-02-12 14:54:09 +0700

    init

---
+++ client/bot.py
@@ -0,0 +1,24 @@
+import tweepy
+
+
+class Bot:
+    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.access_token = access_token
+        self.access_token_secret = access_token_secret
+        self.auth = self.authentication()
+        self.api = tweepy.API(self.auth)
+
+    def authentication(self):
+        self.auth = tweepy.OAuthHandler(
+            self.consumer_key, self.consumer_secret)
+        self.auth.set_access_token(
+            self.access_token, self.access_token_secret)
+        return self.auth
+
+    def get_profile(self):
+        user = self.api.get_user(screen_name="mockthistweet")
+        return user
+
+    # write your method here
