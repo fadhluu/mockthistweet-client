d91b3c57d00ef8fe979e3af0e7333196ee56a9cb
fadhlu <fadhlurahman.i@gmail.com>
2020-02-12 20:33:51 +0700

    fix routes

--- client/urls.py
+++ client/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from . import controller
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('new_tweet', views.new_tweet, name="new tweet")
+    path('', controller.index, name="index"),
+    path('new_tweet', controller.new_tweet, name="new tweet")
 ]
